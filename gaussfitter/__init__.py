a10f1d30a9e418d07472195e2786c0353841e004
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-02-20 14:12:03 +0100

    remove trailing comma

--- gaussfitter/__init__.py
+++ gaussfitter/__init__.py
@@ -1,1 +1,3 @@
-from gaussfitter import collapse_gaussfit, gaussfit, moments, multigaussfit, n_gaussian, onedgaussfit, onedgaussian, onedmoments, twodgaussian,
+from gaussfitter import (collapse_gaussfit, gaussfit, moments, multigaussfit,
+                         n_gaussian, onedgaussfit, onedgaussian, onedmoments,
+                         twodgaussian)
